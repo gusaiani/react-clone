3b6f265d479fbe39b20f7642f6b2744060d3e6c7
gusaiani <gustavo@poe.ma>
2020-09-14 08:41:43 -0300

    Start ReactContext

--- packages/shared/ReactSymbols.js
+++ packages/shared/ReactSymbols.js
@@ -23,6 +23,5 @@ export const REACT_FRAGMENT_TYPE = hasSymbol
 export const REACT_STRICT_MODE_TYPE = hasSymbol
   ? Symbol.for('react.strict_mode')
   : 0xeacc;
-export const REACT_ASYNC_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.async_mode')
-  : 0xeacf;
+export let REACT_PROVIDER_TYPE = 0xeacd;
+export let REACT_CONTEXT_TYPE = 0xeace;
--- packages/shared/ReactTypes.js
+++ packages/shared/ReactTypes.js
@@ -13,3 +13,25 @@ export type RefObject = {|
   value: any,
 |};
 
+export type ReactProviderType<T> = {
+  $$typeof: Symbol | number,
+  context: ReactContext<T>,
+  ...
+};
+
+export type ReactContext<T> = {
+  $$typeof: Symbol | number,
+  Consumer: ReactContext<T>,
+  Provider: ReactProviderType<T>,
+  _calculateChangedBits: ((a: T, b: T) => number) | null,
+  _currentValue: T,
+  _currentValue2: T,
+  _threadCount: number,
+  // DEV only
+  _currentRenderer?: Object | null,
+  _currentRenderer2?: Object | null,
+  // This value may be added by application code
+  // to improve DEV tooling display names
+  displayName?: string,
+  ...
+};
