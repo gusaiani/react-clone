5bfe26f96d74199fd395b6dfc137e75c6b7ad6ef
gusaiani <gustavo@poe.ma>
2020-09-22 09:35:55 -0300

    Continue ReactPartialRenderer

--- packages/react-dom/src/server/ReactPartialRenderer.js
+++ packages/react-dom/src/server/ReactPartialRenderer.js
@@ -15,3 +15,58 @@ import type {ReactProvider, ReactContext} from 'shared/ReactTypes';
 import * as React from 'react';
 import invariant from 'shared/invariant';
 import getComponentName from 'shared/getComponentName';
+
+import {
+  REACT_FRAGMENT_TYPE
+} from 'shared/ReactSymbols'
+
+export type ServerOptions = {
+  identifierPrefix?: string,
+}
+
+// Based on reading the React.Children implementation. TODO: type this somewhere?
+type ReactNode = string | number | ReactElement;
+type FlatReactChildren = Array<null | ReactNode>;
+type toArrayType = (children: mixed) => FlatReactChildren;
+const toArray = ((React.Children.toArray: any): toArrayType);
+
+type Frame = {
+  type: mixed,
+  domNamespace: string,
+  children: FlatReactChildren,
+};
+
+function flattenTopLevelChildren(children: mixed): FlatReactChildren {
+  if (!React.isValidElement(children)) {
+    return toArray(children);
+  }
+  const element = ((children: any): ReactElement);
+  if (element.type !== REACT_FRAGMENT_TYPE)
+}
+
+class ReactDOMServerRenderer {
+  threadID: ThreadID;
+  stack: Array<Frame>;
+  exhausted: boolean;
+  // TODO: type this more strictly:
+  currentSelectValue: any;
+  previousWasTextNode: boolean;
+  makeStaticMarkup: boolean;
+  suspenseDepth: number;
+
+  contextIndex: number;
+  contextStack: Array<ReactContext<any>>;
+  contextValueStack: Array<any>;
+  contextProviderStack: ?Array<ReactProvider<any>>; // DEV-only
+
+  uniqueId: number;
+  identifierPrefix: string;
+
+  constructor(
+    children: mixed,
+    makeStaticMarkup: boolean,
+    options?: ServerOptions,
+  ) {
+    const flatChildren = flattenTopLevelChildren(children);
+  }
+}
